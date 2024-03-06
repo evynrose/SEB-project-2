b059cf0cd4e2fbcfedeca0b577bf04f1eeb870e3
SYED <syedsiddiqui1@hotmail.com>
2024-03-06 16:51:19 +0000

    changes made on home.tsx

---
+++ src/components/Catcards.tsx
@@ -0,0 +1,6 @@
+
+function catCards(){
+
+}
+
+export default catCards
---
+++ src/components/Home.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+import React from "react"
+
+function Home(){
+    return <div>
+       <h2 classname="container is-widescreen">Adopt a Cat</h2>
+    </div>
+
+}
+
+export default Home
+
+// Header 
+// img 
+// <h2> 
+// Body 
+// Footer 
